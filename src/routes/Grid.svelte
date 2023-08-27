f9afc906c07b8e58b8eed5c892e5598b8bf1b9ad
tobseyltd <mail@tobsey.limited>
2023-08-27 08:08:32 +0200

    dispatch found event

--- src/routes/Grid.svelte
+++ src/routes/Grid.svelte
@@ -1,13 +1,40 @@
 <script lang="ts">
 	import Square from './Square.svelte';
 
-    export let grid: string[];
-</script>
-<div class="grid">
-    {#each grid as emoji }
-        <Square  {emoji}/>
-    {/each}
+	export let grid: string[];
 
+	let a: number = -1;
+	let b: number = -1;
+
+	let reset_timeout: number;
+</script>
+
+<div class="grid">
+	{#each grid as emoji, i}
+		<Square
+			{emoji}
+			on:click={() => {
+				clearTimeout(reset_timeout);
+
+				if (a === -1 && b === -1) {
+					a = i;
+				} else if (b === -1) {
+					b = i;
+
+					if (grid[a] === grid[b]) {
+					} else {
+						reset_timeout = setTimeout(() => {
+							a = b = -1;
+						}, 1000);
+					}
+				} else {
+					b = -1;
+					a = i;
+				}
+			}}
+			selected={a === i || b === i}
+		/>
+	{/each}
 </div>
 
 <style>
@@ -16,6 +43,6 @@
 		grid-template-columns: repeat(4, 1fr);
 		grid-template-rows: repeat(4, 1fr);
 		height: 100%;
-        grid-gap: 0.5em
+		grid-gap: 0.5em;
 	}
 </style>
--- src/routes/Square.svelte
+++ src/routes/Square.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	export let emoji: string;
+	export let selected: boolean;
 </script>
 
-<div class="square">
+<div class="square" class:flipped={selected}>
+	<button on:click></button>
 	<span>{emoji}</span>
 </div>
 
@@ -16,5 +18,16 @@
 
 	span {
 		font-size: 5em;
+		pointer-events: none;
+	}
+
+	button {
+		position: absolute;
+		height: 100%;
+		width: 100%;
+	}
+
+	.flipped button {
+		background: red;
 	}
 </style>
